c03d7fdc729356d9e41d3f78c5962dae6928c335
drili <DriliB@hotmail.com>
2023-04-14 19:52:40 +0200

    :hammer: Authentication

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
 const LoginPage = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [redirect, setRedirect] = useState(false)
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch("http://localhost:3001/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: {'Content-Type': "application/json"},
+            credentials: "include"
+        })
+
+        if (response.ok) {
+            setRedirect(true)
+        } else {
+            alert("Wrong credentials!")
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={"/"}></Navigate>
+    }
+
     return (
         <div className='auth-form'>
-            <form action="" className='login'>
+            <form action="" className='login' onSubmit={handleLoginSubmit}>
                 <h1>Login</h1>
                 
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' name="" id="" />
+                <input 
+                    type="text" 
+                    placeholder='username' 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)}/>
+                <input 
+                    type="password" 
+                    placeholder='password' 
+                    name="" 
+                    id="" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}/>
                 <button>Login</button>
             </form>
         </div>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleRegisterSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch("http://localhost:3001/register", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: {'Content-Type': "application/json"}
+        })
+
+        if (response.status === 200) {
+            alert("Registration was successful!")
+        } else {
+            alert("Registration failed! Try again later")
+        }
+    }
+
     return (
-        <div className='auth-form'>
+        <div className='auth-form' onSubmit={handleRegisterSubmit}>
             <form action="" className='register'>
                 <h1>Register</h1>
                 
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' name="" id="" />
-                <button>Register</button>
+                <input 
+                    type="text" 
+                    placeholder='username' 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder='password'
+                    name="" 
+                    id=""
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Register</button>
             </form>
         </div>
     )
