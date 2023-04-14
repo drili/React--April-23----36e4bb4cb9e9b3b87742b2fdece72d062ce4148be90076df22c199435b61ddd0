eeead88ee5f0c23e31d433c3b53630ef9cf7fd0c
drili <DriliB@hotmail.com>
2023-04-14 20:07:02 +0200

    :hammer: Logged in / logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, json } from 'react-router-dom'
 
 const Header = () => {
+    const [username, setUsername] = useState("")
+
+    useEffect(() => {
+        fetch("http://localhost:3001/profile", {
+            credentials: "include"
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username)
+            })
+        })
+    }, [])
+
+    const handleLogout = () => {
+        fetch("http://localhost:3001/logout", {
+            credentials: "include",
+            method: "POST"
+        })
+
+        setUsername("")
+    }
+
     return (
         <header>
             <Link className='logo' to="/">Blog</Link>
             
             <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {username && (
+                    <>
+                        <Link to="/create">Create New Post</Link>
+                        <a onClick={handleLogout}>Logout</a>
+                    </>
+                )}
+                
+                {!username && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
             </nav>
         </header>
     )
